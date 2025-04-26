280d26576319054bf37f4cfa0700afe64f03a6ab
siyeon9302 <siyeon9302@gmail.com>
2025-04-26 23:53:39 +0900

    feat: week3-1

--- week3/week3-1/src/components/Card.jsx
+++ week3/week3-1/src/components/Card.jsx
@@ -2,12 +2,12 @@
 
 import style from './Card.module.css';
 
-const Card = () => {
+const Card = ({name, github, englishName}) => {
   return (
     <div className={style.card}>
-      <p>이름: 김스웹</p>
-      <p>깃허브: abc</p>
-      <p>영문이름: Kim Sweb</p>
+      <p>이름: {name}</p>
+      <p>깃허브: {github}</p>
+      <p>영문이름: {englishName}</p>
     </div>
   );
 };
